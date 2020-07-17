18fc2f267548e4c53df48f555cbd59b1616f6b1a
Shivam <shivam@excellencetechnologies.info>
2020-07-17 14:00:10 +0530

    Todo-custom

--- src/App.js
+++ src/App.js
@@ -1,54 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
+import UseHook from "./hooks/UseHook";
 import TodoForm from "./TodoForm";
 import Todo from "./components/Todo";
 import Filter from "./components/Filter";
 import "./App.css";
 
 function App() {
-  const [state, setState] = useState({
-    filter: "",
-    todos: [
-      { text: "Rice", checked: false },
-      { text: "Oil", checked: false },
-      { text: "Beans", checked: false },
-    ],
-  });
-  const addTodo = (text) => {
-    const newstate = { ...state, todos: [...state.todos, { text }] };
-    setState(newstate);
-  };
-  const changervalue = (e, index) => {
-    const newstate = [...state.todos];
-    newstate[index].text = e.target.value;
-    setState({ ...state, state: newstate });
-  };
-  const deleteTodo = (index) => {
-    const newstate = [...state.todos];
-    newstate.splice(index, 1);
-    setState({ ...state, todos: newstate });
-  };
-  const completeTodo = (index) => {
-    const newstate = [...state.todos];
-    newstate[index].checked = !newstate[index].checked;
-    setState({ ...state, state: newstate });
-  };
+  const custmhook = UseHook();
   return (
     <div class=" hello">
       <div>
         <h3>TODO APP</h3>
-        <TodoForm addTodo={addTodo} />
+        <TodoForm addTodo={custmhook.addTodo} />
         <br />
         <Filter
           onClick={(text) => {
-            setState({ ...state, filter: text });
+            custmhook.setState({ ...custmhook.state, filter: text });
           }}
         />
         <br />
-        {state.todos
+        {custmhook.state.todos
           .filter((data) =>
-            state.filter === "completed"
+            custmhook.state.filter === "completed"
               ? data.checked
-              : state.filter === "incompleted"
+              : custmhook.state.filter === "incompleted"
               ? !data.checked
               : data
           )
@@ -59,9 +34,9 @@ function App() {
                 key={index}
                 index={index}
                 todo={todo}
-                completeTodo={completeTodo}
-                changervalue={changervalue}
-                deleteTodo={deleteTodo}
+                completeTodo={custmhook.completeTodo}
+                changervalue={custmhook.changervalue}
+                deleteTodo={custmhook.deleteTodo}
               />
               <br />
             </>
--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
+import UseHook from "./hooks/UseHook";
 
 function TodoForm({ addTodo }) {
-  const [value, setValue] = useState("");
+  const custmhook = UseHook();
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (value === "") {
+    if (custmhook.value === "") {
       alert("you must write something");
     }
-    if (!value) return;
-    addTodo(value);
-    setValue("");
+    if (!custmhook.value) return;
+    addTodo(custmhook.value);
+    custmhook.setValue("");
   };
   return (
     <form onSubmit={handleSubmit} style={{ marginLeft: 15 }}>
       <input
         type="text"
         className="input"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
+        value={custmhook.value}
+        onChange={(e) => custmhook.setValue(e.target.value)}
       />{" "}
       <button>ADD</button>
     </form>
